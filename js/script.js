e576907fb67418d92e504d95a1faf09711a63056
Andrenord282 <79256226+Andrenord282@users.noreply.github.com>
2021-06-21 20:23:55 +0300

    Добавил иконочный шрифт, свг фон,  корректировка адаптации на телефон и планшет

--- js/script.js
+++ js/script.js
@@ -18,12 +18,18 @@ navigationLink.forEach(function (link, i) {
        navigationLink.forEach(function (active, i) {
            if (active.classList.contains('active')){
                active.classList.remove('active');
+			   
                navNum[i].classList.remove('active');
+			   dot[i].classList.remove('active');
+
            };
        });
        link.classList.add('active');
        navNum[i].classList.add('active');
-       
+	   dot[i].classList.add('active');
+
+
+
 
        btnBurgerNavigation.classList.remove('active');
        navigationBody.classList.remove('active');
@@ -140,42 +146,42 @@ btnMap.addEventListener('click', () =>{
 
 // Работа анимации блоков
 
-const animItems = document.querySelectorAll('.anim-items');
+// const animItems = document.querySelectorAll('.anim-items');
 
-if (animItems.length > 0) {
-	window.addEventListener('scroll', animOnScroll);
-	function animOnScroll() {
-		for (let index = 0; index < animItems.length; index++) {
-			const animItem = animItems[index];
-			const animItemHeight = animItem.offsetHeight;
-			const animItemOffset = offset(animItem).top;
-			const animStart = 4;
+// if (animItems.length > 0) {
+// 	window.addEventListener('scroll', animOnScroll);
+// 	function animOnScroll() {
+// 		for (let index = 0; index < animItems.length; index++) {
+// 			const animItem = animItems[index];
+// 			const animItemHeight = animItem.offsetHeight;
+// 			const animItemOffset = offset(animItem).top;
+// 			const animStart = 4;
 
-			let animItemPoint = window.innerHeight - animItemHeight / animStart;
-			if (animItemHeight > window.innerHeight) {
-				animItemPoint = window.innerHeight - window.innerHeight / animStart;
-			}
+// 			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+// 			if (animItemHeight > window.innerHeight) {
+// 				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+// 			}
 
-			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-				animItem.classList.add('active');
-			} else {
-				if (!animItem.classList.contains('_anim-no-hide')) {
-					animItem.classList.remove('active');
-				}
-			}
-		}
-	}
-	function offset(el) {
-		const rect = el.getBoundingClientRect(),
-			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-	}
+// 			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+// 				animItem.classList.add('active');
+// 			} else {
+// 				if (!animItem.classList.contains('_anim-no-hide')) {
+// 					animItem.classList.remove('active');
+// 				}
+// 			}
+// 		}
+// 	}
+// 	function offset(el) {
+// 		const rect = el.getBoundingClientRect(),
+// 			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+// 			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// 		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+// 	}
 
-	setTimeout(() => {
-		animOnScroll();
-	}, 300);
-}
+// 	setTimeout(() => {
+// 		animOnScroll();
+// 	}, 300);
+// }
 
 
 
